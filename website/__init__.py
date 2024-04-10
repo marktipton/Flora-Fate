3facdca0ac88f2e8a7a0787ea0d57029d462f06a
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-09 21:17:28 -0500

    made several changes getting DB up. Moved styles and scripts into static folder

--- website/__init__.py
+++ website/__init__.py
@@ -11,22 +11,24 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'bloomin onions'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config["SECRET_KEY"] = "bloomin onions"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    # app.register_blueprint(views, url_prefix='/')
+    # app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
 
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -36,10 +38,9 @@ def create_app():
     return app
 
 
-# Bug found here
 def create_database(app):
     db_path = os.path.join(app.root_path, DB_NAME)
     if not path.exists(db_path):
         with app.app_context():
             db.create_all()
-        print('Created Database!')
+        print("Created Database!")
